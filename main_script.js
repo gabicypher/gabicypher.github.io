ab9a1e3db0ef570c03c6a5e2d3bd630ce0550047
gabicypher <125352594+gabicypher@users.noreply.github.com>
2024-06-19 16:58:21 -0600

    Redirected Neocities links

--- main_script.js
+++
@@ -1,3 +0,0 @@
-import { emojiCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
-
-new emojiCursor({ emoji: [":3", ":D", ">:)", "❤️", "💛", "🎨", "💥", "💫", "🪐", "⭐️", "✨", "⚡️", "🌈", "☀️"] });
