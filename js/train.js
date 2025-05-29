357bd3fc1f8914adf1c958398b6715557139dec0
gabicypher <125352594+gabicypher@users.noreply.github.com>
2025-05-29 00:26:52 -0600

    Added homepage train game

---
+++ js/train.js
@@ -0,0 +1,217 @@
+// This code runs the home page train animation
+
+// When the user clicks on div, open the popup and pause the train
+function train1Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "visible"; // make popup visible
+    train2popup.style.visibility = "hidden"; // hide other popups
+    train3popup.style.visibility = "hidden";
+    train4popup.style.visibility = "hidden";
+    train5popup.style.visibility = "hidden";
+    train6popup.style.visibility = "hidden";
+    train7popup.style.visibility = "hidden";
+    train8popup.style.visibility = "hidden";
+  }
+  
+  function train2Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "hidden";
+    train2popup.style.visibility = "visible";
+    train3popup.style.visibility = "hidden";
+    train4popup.style.visibility = "hidden";
+    train5popup.style.visibility = "hidden";
+    train6popup.style.visibility = "hidden";
+    train7popup.style.visibility = "hidden";
+    train8popup.style.visibility = "hidden";
+  }
+  
+  function train3Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "hidden";
+    train2popup.style.visibility = "hidden";
+    train3popup.style.visibility = "visible";
+    train4popup.style.visibility = "hidden";
+    train5popup.style.visibility = "hidden";
+    train6popup.style.visibility = "hidden";
+    train7popup.style.visibility = "hidden";
+    train8popup.style.visibility = "hidden";
+  }
+  
+  function train4Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "hidden";
+    train2popup.style.visibility = "hidden";
+    train3popup.style.visibility = "hidden";
+    train4popup.style.visibility = "visible";
+    train5popup.style.visibility = "hidden";
+    train6popup.style.visibility = "hidden";
+    train7popup.style.visibility = "hidden";
+    train8popup.style.visibility = "hidden";
+  }
+  
+  function train5Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "hidden";
+    train2popup.style.visibility = "hidden";
+    train3popup.style.visibility = "hidden";
+    train4popup.style.visibility = "hidden";
+    train5popup.style.visibility = "visible";
+    train6popup.style.visibility = "hidden";
+    train7popup.style.visibility = "hidden";
+    train8popup.style.visibility = "hidden";
+  }
+  
+  function train6Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "hidden";
+    train2popup.style.visibility = "hidden";
+    train3popup.style.visibility = "hidden";
+    train4popup.style.visibility = "hidden";
+    train5popup.style.visibility = "hidden";
+    train6popup.style.visibility = "visible";
+    train7popup.style.visibility = "hidden";
+    train8popup.style.visibility = "hidden";
+  }
+  
+  function train7Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "hidden";
+    train2popup.style.visibility = "hidden";
+    train3popup.style.visibility = "hidden";
+    train4popup.style.visibility = "hidden";
+    train5popup.style.visibility = "hidden";
+    train6popup.style.visibility = "hidden";
+    train7popup.style.visibility = "visible";
+    train8popup.style.visibility = "hidden";
+  }
+  
+  function train8Function() {
+    var train1popup = document.getElementById("train1Popup");
+    var train2popup = document.getElementById("train2Popup");
+    var train3popup = document.getElementById("train3Popup");
+    var train4popup = document.getElementById("train4Popup");
+    var train5popup = document.getElementById("train5Popup");
+    var train6popup = document.getElementById("train6Popup");
+    var train7popup = document.getElementById("train7Popup");
+    var train8popup = document.getElementById("train8Popup");
+    train1popup.style.visibility = "hidden";
+    train2popup.style.visibility = "hidden";
+    train3popup.style.visibility = "hidden";
+    train4popup.style.visibility = "hidden";
+    train5popup.style.visibility = "hidden";
+    train6popup.style.visibility = "hidden";
+    train7popup.style.visibility = "hidden";
+    train8popup.style.visibility = "visible";
+  }
+  
+  // When the user clicks the x, close the popup and restart the train
+  function close1Function() {
+    var trainpopup = document.getElementById("train1Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  function close2Function() {
+    var trainpopup = document.getElementById("train2Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  function close3Function() {
+    var trainpopup = document.getElementById("train3Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  function close4Function() {
+    var trainpopup = document.getElementById("train4Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  function close5Function() {
+    var trainpopup = document.getElementById("train5Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  function close6Function() {
+    var trainpopup = document.getElementById("train6Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  function close7Function() {
+    var trainpopup = document.getElementById("train7Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  function close8Function() {
+    var trainpopup = document.getElementById("train8Popup");
+    trainpopup.style.visibility = "hidden";
+  }
+  
+  // potential code for pausing animation
+    //var train = document.getElementById("train");
+    //train.style.animationPlayState = "paused"; // pause animation. slightly different syntax due to no dashes in js
+    //document.querySelector('.illustrations').innerHTML += '<div class="card">' + '<p>WORDS WORDS WORDS</p>' + window.getComputedStyle("trainpopup") + '</div>';
+    //train.style.animationPlayState = "running"; // run animation. slightly different syntax due to no dashes in js
+  
+  
+  // on hover, the train animation pauses
+  //function hoverFunction () {
+  //  var train = document.getElementById("train");
+  //  train.style.animationPlayState = "paused"; // pause animation
+  //}
+  
+  // on mouse away, the train animation runs
+  //function unhoverFunction () {
+  //  var trainpopup = document.getElementById("trainPopup");
+  //  var train = document.getElementById("train");
+  //  if (trainpopup.getComputedStyle.getPropertyValue("visibility") == "hidden") {
+  //    train.style.animationPlayState = "running"; // run animation
+  //  }
+  //}  
