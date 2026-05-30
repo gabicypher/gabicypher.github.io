bf0e6a488359f16d52723343d541ade4efcc357d
gabicypher <125352594+gabicypher@users.noreply.github.com>
2026-05-29 21:03:39 -0600

    Fixed JS issues on train, music player, and footer

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,1 @@
-// div class "illustrations" inserts <div class="card"> <img src="images[i]"> <p>dateslist[i]</p> </div>
-for (let i = 0; i < dateslist.length; i++) {
-    document.querySelector('.illustrations').innerHTML += '<div class="card">' + '<img src="' + images[i] + '">' + '<p>' + dateslist[i] + '</p>' + '</div>';
-}
-
-document.querySelector('.footer').innerHTML += '<p>&copy; 2023-2026 Gabi Seifert. Website made entirely from scratch with HTML, CSS, and a dash of Javascript.</p>';
+document.querySelector('.footer').innerHTML += 'Gabi Seifert, 2023-2026. Website made entirely from scratch with HTML, CSS, and a dash of Javascript.</p>';
